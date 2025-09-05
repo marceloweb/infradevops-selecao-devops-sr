182549609df86b10763745db07fec7720b97c0b3
Marcelo Oliveira <marcelojppb@gmail.com>
2025-09-05 16:22:40 -0300

    Fix: RDS

--- terraform/modules/eks_cluster/outputs.tf
+++ terraform/modules/eks_cluster/outputs.tf
@@ -7,3 +7,8 @@ output "cluster_endpoint" {
   description = "The endpoint for the EKS cluster"
   value       = aws_eks_cluster.eks.endpoint
 }
+
+output "cluster_security_group_id" {
+  description = "The ID of the EKS cluster's security group."
+  value       = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
+}
