7e60858862c85b80001b151161de12d5678a5ac3
Marcelo Oliveira <marcelojppb@gmail.com>
2025-09-05 17:59:37 -0300

    Adjustment db engine version

--- terraform/modules/rds/variables.tf
+++ terraform/modules/rds/variables.tf
@@ -35,7 +35,7 @@ variable "db_name" {
 variable "db_engine_version" {
   description = "The engine version of the database."
   type        = string
-  default     = "17.4-R1"
+  default     = "17.4"
 }
 
 variable "eks_cluster_security_group_id" {
