854f819b9eb4a0f09eee57262406ab3026746a12
Marcelo Oliveira <marcelojppb@gmail.com>
2025-09-06 12:19:34 -0300

    Fix: Get the database secrets

--- comments-api/app/__init__.py
+++ comments-api/app/__init__.py
@@ -9,7 +9,13 @@ def create_app(test_config=None):
     if test_config:
         app.config.from_mapping(test_config)
     else:
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@db:5432/comments_db'
+        db_user = os.getenv('POSTGRES_USER', 'postgres')
+        db_password = os.getenv('POSTGRES_PASSWORD', 'password')
+        db_host = os.getenv('POSTGRES_HOST', 'db')
+        db_port = os.getenv('POSTGRES_PORT', '5432')
+        db_name = os.getenv('POSTGRES_DB', 'comments_db')
+
+        app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
